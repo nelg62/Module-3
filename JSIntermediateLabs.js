eba138101491f7fd202a3cd481d1c16b923d0e1f
nelg62 <glen.harding.nz@gmail.com>
2024-03-20 11:00:02 +1300

    Question 6

--- JSIntermediateLabs.js
+++ JSIntermediateLabs.js
@@ -243,5 +243,12 @@ console.log(0.3 == currencyOperation(0.1, 0.2, '+')) // true
 
 const colours = ['red', 'green', 'blue', 'yellow', 'orange', 'red', 'blue', 'yellow']
 const testScores = [55, 84, 97, 63, 55, 32, 84, 91, 55, 43]
+const peopleNames = ['Glen', 'Dave', 'Fred', 'Jessie', 'Glen', 'Fred', 'Jessie', 'Mac']
+
+function unique(duplicatesArray) {
+  console.log(Array.from(new Set(duplicatesArray)))
+}
+
 console.log(unique(colours)) // [ 'red', 'green', 'blue', 'yellow', 'orange' ]
 console.log(unique(testScores)) // [ 55, 84, 97, 63, 32, 91, 43 ]
+console.log(unique(peopleNames))
