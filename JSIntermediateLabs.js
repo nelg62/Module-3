8b0640091bb02c34a4e0fcfb2d44e4070ca36f12
nelg62 <glen.harding.nz@gmail.com>
2024-03-20 12:49:30 +1300

    question 7

--- JSIntermediateLabs.js
+++ JSIntermediateLabs.js
@@ -252,3 +252,74 @@ function unique(duplicatesArray) {
 console.log(unique(colours)) // [ 'red', 'green', 'blue', 'yellow', 'orange' ]
 console.log(unique(testScores)) // [ 55, 84, 97, 63, 32, 91, 43 ]
 console.log(unique(peopleNames))
+
+
+// ---------------------------------------------------------question 7
+
+// Use the following array of book objects to practice the array functions for map, find and
+// filter. Test each of your answers to the below tasks.
+
+const books = [
+  { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', year: 1925 },
+  { id: 2, title: 'To Kill a Mockingbird', author: 'Harper Lee', year: 1960 },
+  { id: 3, title: '1984', author: 'George Orwell', year: 1949 },
+  { id: 4, title: 'Brave New World', author: 'Aldous Huxley', year: 1932 },
+  { id: 5, title: 'The Catcher in the Rye', author: 'J.D. Salinger', year: 1951 },
+  ];
+
+//   a) Write a function getBookTitle(bookId) that uses the find function to return the
+// title of the book object with the matching id.
+
+function getBookTitle(bookId) {
+  let idNum = books.find(book => book.id == bookId)
+  console.log(idNum)
+}
+
+getBookTitle(1)
+
+// b) Write a function getOldBooks() that uses the filter function to return all book
+// objects written before 1950.
+
+function getOldBooks() {
+  let oldBooks = books.filter(book => book.year < 1950)
+  console.log(oldBooks)
+}
+
+getOldBooks()
+
+// c) Write a function addGenre() that uses the map function to add a new genre property
+// to all of the above books, with the value ‘classic’.
+
+function addGenre() {
+  let newGenres = books.map(book => ({...book, value: "Classic"}) )
+  console.log(newGenres)
+}
+
+addGenre()
+
+// d) (Extension) Write a function getTitles(authorInitial) that uses map and
+// filter together to return an array of book titles for books written by authors whose
+// names start with authorInitial.
+
+function getTitles(authorInitial) {
+  let bookFilter = books.filter(book => book.author.charAt(0).toUpperCase() == authorInitial.toUpperCase())
+  let bookMap = bookFilter.map(book => book.title)
+  console.log(bookMap)
+}
+
+getTitles('j')
+
+// e) (Extension) Write a function latestBook() that uses find and forEach to get the
+// book with the most recent publication date.
+biggestYear = []
+function latestBook() {
+  books.forEach((item) => {
+    biggestYear.push(item.year)
+  })
+console.log(biggestYear)
+let chosenNum = Math.max(...biggestYear)
+  let maxYear = books.find(book => book.year == chosenNum)
+  console.log(maxYear)
+}
+
+latestBook()
