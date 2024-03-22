a06a7fb5f0d5922af29efb7f2beff4dc693e56a2
nelg62 <glen.harding.nz@gmail.com>
2024-03-23 11:06:40 +1300

    advancesd js page 6 test

--- index.js
+++ index.js
@@ -1,26 +1,38 @@
-console.log(1/0) // division by zero = Infinity
-console.log(-1/0) // negative division by zero = -Infinity
-console.log("not a number" / 1) // invalid mathematical operation = NaN
+// console.log(1/0) // division by zero = Infinity
+// console.log(-1/0) // negative division by zero = -Infinity
+// console.log("not a number" / 1) // invalid mathematical operation = NaN
 
-// // function checkAge returns a value when called
-// function checkAge(age) {
-//     if (age >= 18) {
-//     return 'adult'; // if the condition is true, return this string and exit
+// // // function checkAge returns a value when called
+// // function checkAge(age) {
+// //     if (age >= 18) {
+// //     return 'adult'; // if the condition is true, return this string and exit
+// //     }
+// //     return 'non-adult'; // if the condition was false, return this string and exit
+// //     }
+// //     console.log( checkAge(21) ) // adult
+// //     console.log( checkAge(13) ) // non-adult
+
+//     const checkAge = (age) => {
+//     return (age >= 18 ? 'adult' : 'non-adult')
 //     }
-//     return 'non-adult'; // if the condition was false, return this string and exit
+
+//     console.log(checkAge(20))
+
+//     function test(value) {
+//         return value || "not a value"
 //     }
-//     console.log( checkAge(21) ) // adult
-//     console.log( checkAge(13) ) // non-adult
 
-    const checkAge = (age) => {
-    return (age >= 18 ? 'adult' : 'non-adult')
-    }
+//     console.log(test('apple'))
+//     console.log(test())
 
-    console.log(checkAge(20))
 
-    function test(value) {
-        return value || "not a value"
-    }
+function increaseFactory(x) {
+    return function internalFactory() {
+       x++
+       return x;
+    };
+}
 
-    console.log(test('apple'))
-    console.log(test())
+const people =  increaseFactory(30)
+console.log(people())
+console.log(people())
