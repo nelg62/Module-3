e9a2518c7b621ac8cee1826dd47e954d927d8a5e
nelg62 <glen.harding.nz@gmail.com>
2024-03-25 19:46:26 +1300

    bind

--- index.js
+++ index.js
@@ -26,13 +26,41 @@
 //     console.log(test())
 
 
-function increaseFactory(x) {
-    return function internalFactory() {
-       x++
-       return x;
-    };
-}
+// function increaseFactory(x) {
+//     return function internalFactory() {
+//        x++
+//        return x;
+//     };
+// }
 
-const people =  increaseFactory(30)
-console.log(people())
-console.log(people())
+// const people =  increaseFactory(30)
+// console.log(people())
+// console.log(people())
+
+
+function cachingDecorator(origFunction) { // decorator takes a function as parameter
+    const cache = new Map(); // can also include outer environment variables via a closure
+    return function(x) { // decorator returns same function with extra bits - caching
+    if (cache.has(x)) { // if the key exists in the cache,
+    console.log('returned cached value for '+x); return cache.get(x); // read and return the result from it
+    }
+    let result = origFunction(x) // otherwise, call the original function and store the result
+    cache.set(x, result); // then cache (remember) the result for next time
+    return result;
+    };
+    }
+
+let worker = {
+    getMultiplier() {
+    return Math.floor(Math.random() * 1_000_000); // large random number
+    },
+    slow(x) {
+    let random = 0, goal = x * this.getMultiplier(); // needs context to work
+    for (let i = 0; i < goal; i++) random++;
+    console.log(`worker.slow(${x}): randomly generated goal is ${goal}`);
+    return random; // return large number
+    }
+    };
+    worker.slow(5) // works, context comes from before the dot, ie. worker
+    worker.fast = cachingDecorator(worker.slow.bind(worker)) // without call/apply, context is lost
+    worker.fast(3) // TypeError: Cannot read properties of undefined (reading 'getMultiplier')
