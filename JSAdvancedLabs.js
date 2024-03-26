d896f48b581063f0219160ff497c2afdd7888f25
nelg62 <glen.harding.nz@gmail.com>
2024-03-26 15:17:15 +1300

    question 4

--- JSAdvancedLabs.js
+++ JSAdvancedLabs.js
@@ -61,52 +61,92 @@
 
 
 // ------------------------------question 3
-console.log('This is question 3')
+// console.log('This is question 3')
 
-// 'Debouncing' is a concept that refers to 'putting off' the execution of multiple, fast-timed,
-// similar requests until there's a brief pause, then only executing the most recent of those
-// requests. See https://www.techtarget.com/whatis/definition/debouncing
-// It's often used to handle fast-firing scrolling events in a browser, or to prevent multiple server
-// requests being initiated if a user clicks repeatedly on a button.
-// Using the following code to test and start with:
-// a) Create a debounce(func) decorator, which is a wrapper that takes a function func and
-// suspends calls to func until there's 1000 milliseconds of inactivity. After this 1 second
-// pause, the most recent call to func should be executed and any others ignored.
+// // 'Debouncing' is a concept that refers to 'putting off' the execution of multiple, fast-timed,
+// // similar requests until there's a brief pause, then only executing the most recent of those
+// // requests. See https://www.techtarget.com/whatis/definition/debouncing
+// // It's often used to handle fast-firing scrolling events in a browser, or to prevent multiple server
+// // requests being initiated if a user clicks repeatedly on a button.
+// // Using the following code to test and start with:
+// // a) Create a debounce(func) decorator, which is a wrapper that takes a function func and
+// // suspends calls to func until there's 1000 milliseconds of inactivity. After this 1 second
+// // pause, the most recent call to func should be executed and any others ignored.
 
-// function debounce(func) {
+// // function debounce(func) {
+// //     let timeout
+
+// //     return () => {
+// //         clearTimeout(timeout)
+// //         timeout = setTimeout(() => {
+// //             func.apply()
+// //         },1000)
+// //     }
+// // }
+
+// // b) Extend the debounce decorator function to take a second argument ms, which defines the
+// // length of the period of inactivity instead of hardcoding to 1000ms
+
+// function debounce(func, ms, msg) {
 //     let timeout
 
 //     return () => {
 //         clearTimeout(timeout)
 //         timeout = setTimeout(() => {
-//             func.apply()
-//         },1000)
+//            funcbind = func.bind()
+//            return funcbind(msg)
+//         },ms)
 //     }
 // }
 
-// b) Extend the debounce decorator function to take a second argument ms, which defines the
-// length of the period of inactivity instead of hardcoding to 1000ms
+// // c) Extend debounce to allow the original debounced function printMe to take an argument
+// // msg which is included in the console.log statement.
 
-function debounce(func, ms, msg) {
-    let timeout
+// function printMe(msg) {
+//     console.log(`printing debounced message ${msg}`)
+//     }
+//     printMe = debounce(printMe, 1000, 'this is an added message'); //create this debounce function for a)
+//     //fire off 3 calls to printMe within 300ms - only the LAST one should print, after 1000ms of no calls
+//     setTimeout( printMe, 100);
+//     setTimeout( printMe, 200);
+//     setTimeout( printMe, 300);
 
-    return () => {
-        clearTimeout(timeout)
-        timeout = setTimeout(() => {
-           funcbind = func.bind()
-           return funcbind(msg)
-        },ms)
-    }
+
+    // ------------------------------question 4
+    console.log('This is question 4')
+
+//     The Fibonacci sequence of numbers is a famous pattern where the next number in the
+// sequence is the sum of the previous 2.
+// e.g. 1, 1, 2, 3, 5, 8, 13, 21, 34, etc.
+// a) Write a function printFibonacci() using setInterval that outputs a number in
+// the Fibonacci sequence every second.
+
+// function printFibonacci() {
+//     let firstNum = 0
+//     let secondNum = 1
+//     let sequence = setInterval(() => {numTogether = firstNum + secondNum
+//     firstNum = secondNum
+//     secondNum = numTogether 
+//     console.log(numTogether)},1000)
+//     // return sequence
+// }
+// printFibonacci()
+
+// b) Write a new version printFibonacciTimeouts() that uses nested setTimeout
+// calls to do the same thing
+
+function printFibonacciTimeouts(time,limit) {
+        let firstNum = 0
+        let secondNum = 1
+    setTimeout(function fibloop(loopnum) {numTogether = firstNum + secondNum
+            firstNum = secondNum
+            secondNum = numTogether 
+            console.log(numTogether)
+        if (loopnum < limit) setTimeout(fibloop, time, loopnum+1)
+        },time, 1)
 }
 
-// c) Extend debounce to allow the original debounced function printMe to take an argument
-// msg which is included in the console.log statement.
+printFibonacciTimeouts(1000, 10)
 
-function printMe(msg) {
-    console.log(`printing debounced message ${msg}`)
-    }
-    printMe = debounce(printMe, 1000, 'this is an added message'); //create this debounce function for a)
-    //fire off 3 calls to printMe within 300ms - only the LAST one should print, after 1000ms of no calls
-    setTimeout( printMe, 100);
-    setTimeout( printMe, 200);
-    setTimeout( printMe, 300);
+// c) Extend one of the above functions to accept a limit argument, which tells it how many
+// numbers to print before stopping.
